98cb42a577124ee97a6ad059bb721472ecf75848
Sanda Golcea <sanda.golcea@gmail.com>
2015-01-03 18:31:09 +0000

    Adds sessions

--- lib/tic-tac-toe.rb
+++ lib/tic-tac-toe.rb
@@ -3,22 +3,36 @@ require_relative 'player'
 require_relative 'board'
 
 class TicTacToe < Sinatra::Base
+  enable :sessions
+
   get '/' do
     erb :index
   end
 
-  post '/fight' do
+  post '/game' do
      first = params[:firstplayer]
      second = params[:secondplayer]
-    
+      
     @player1 = Player.new(first,"X")
     @player2 = Player.new(second,"0")
     "Hello #{@player1.name} and #{@player2.name}"
+    
+    @current_player = [@player1,@player2].sample
 
     @board = Board.new
-    erb :fight
+
+    session[:player1] = @player1
+    session[:player2] = @player2
+    session[:current_player] = @current_player
+    session[:board] = @board
+
+    erb :game
   end
 
+  post '/game/move' do
+    'Hola bola'
+    session[:player1].name
+  end
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
